cdcc3d4d7c0c1fe227d67ae64c1bb8dd87283488
abgoo <abgoo@AMMAR-PC>
2023-09-07 01:15:31 +0300

    Add project files.

---
+++ Boolean Expresions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Boolean_Expresions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
